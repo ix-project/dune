e0b4d9aab29788b26a46f9f2461588eea231938c
ustiugov <dmitrii.ustiugov@epfl.ch>
2018-08-20 12:53:37 +0200

    made dune easy to integrate with C++ projects (i.e., extern C pragmas added to headers) (#11)

--- libdune/dune.h
+++ libdune/dune.h
@@ -12,6 +12,10 @@
 #include "fpu.h"
 #include "../kern/dune.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef void (*sighandler_t)(int);
 
 // utilities
@@ -356,3 +360,7 @@ static inline int dune_init_and_enter(void)
 }
 
 extern void dune_control_guest_ints(bool enable);
+
+#ifdef __cplusplus
+}
+#endif
